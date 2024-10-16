cf53ba61a19a239e3cf5d91e8c3e6b43e0b28b07
LucaCampusfp <lucacampusfp@gmail.com>
2024-10-16 14:25:08 +0200

    subir

--- Ej_14_Abrir_ventanas/index.js
+++ Ej_14_Abrir_ventanas/index.js
@@ -1,3 +1,14 @@
-function abrirURL(URL){
-    var nuevaVentana = nuevaVentana = window.open(URL,"","height=10%", "width=100%");
+function abrirURL(URL, x , y) {
+    var nuevaVentana = window.open(URL, "", `height=1000,width=620,left=${x},top=${y}`);
 }
+
+//A LA HORA DE ESTABLCER MEDIDAS MEJOR USAR COMILLAS SIMPLES
+//ALternativa
+/*
+function abrirURL(URL, x, y) {
+    var nuevaVentana = window.open(URL, "", "height=1000,width=600,left=" + x + ",top=" + y);
+}
+*/
+//El símbolo $ se utiliza dentro de comillas invertidas (`) 
+//para insertar el valor de una variable en una cadena. 
+//Esto significa que puedes combinar texto y variables fácilmente.
