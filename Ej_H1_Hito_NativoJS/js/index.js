3956febf3f86bf30b37da2957a5e2543d4fcab96
LucaCampusfp <lucacampusfp@gmail.com>
2024-10-27 00:37:07 +0200

    correcion

--- Ej_H1_Hito_NativoJS/js/index.js
+++ Ej_H1_Hito_NativoJS/js/index.js
@@ -1,28 +1,27 @@
-
 function manejarEnvioFormulario(event) {
     event.preventDefault();  // Previene el comportamiento por defecto del formulario
+
     // Captura de valores del formulario
     var nombre = document.getElementById('nombre').value;
     var apellido = document.getElementById('apellido').value;
     var usuario = document.getElementById('usuario').value;
     var contraseña = document.getElementById('contraseña').value;
-    var confContraseñaInput = document.getElementById('conf_contraseña'); // Aquí se corrige la referencia
+    var confContraseñaInput = document.getElementById('conf_contraseña'); // Referencia correcta
     var conf_contraseña = confContraseñaInput.value; // Usa la referencia correcta
     var correo = document.getElementById('correo').value;
     var paises = document.getElementById('paises').value;
     var comentario = document.getElementById('comentario').value;
     var option = document.getElementById('option').value;
 
+    // Verifica si las contraseñas coinciden
     if (contraseña !== conf_contraseña) {
-        confContraseñaInput.setCustomValidity("Las contraseñas no coinciden. Por favor, inténtalo de nuevo.");
-        confContraseñaInput.reportValidity(); // Muestra el mensaje de error
-        return;
-    } else {
-        confContraseñaInput.setCustomValidity(""); // Resetea el mensaje de error si las contraseñas coinciden
+        alert("Error: las contraseñas no coinciden");
+        document.getElementById('contraseña').value = ''; 
+        confContraseñaInput.value = ''; 
+        return; 
     }
-   
-   
- // Crear un objeto con los datos del formulario
+    
+    // Si las contraseñas coinciden, crea un objeto con los datos del formulario
     var datosFormulario = {
         nombre: nombre,
         apellido: apellido,
@@ -36,6 +35,7 @@ function manejarEnvioFormulario(event) {
     // Almacenar el objeto en localStorage como una cadena JSON
     localStorage.setItem('datosFormulario', JSON.stringify(datosFormulario));
 
+    // Redirigir a la nueva página
     window.location.href = 'index2.html';
 }
 
